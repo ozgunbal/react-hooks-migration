81b75d6d80aeb71ac6f2acbcbc874286413993cf
Ozgun Bal <ozgunbal@gmail.com>
2019-12-16 10:20:49 +0300

    made hook migration

--- src/ApplicationStats.js
+++ src/ApplicationStats.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const ApplicationStats = ({ formData, location }) => (
-  <div style={{ display: "flex", justifyContent: "center" }}>
-    <div className="card" style={{ width: "18rem" }}>
-      <div className="card-body">
-        <h5 className="card-title">Form Data</h5>
-        <p className="card-text">{`${JSON.stringify(formData, null, 2)}`}</p>
+const ApplicationStats = () => {
+  const formData = useSelector(state => state);
+  const location = useLocation();
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className="card" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">Form Data</h5>
+          <p className="card-text">{`${JSON.stringify(formData, null, 2)}`}</p>
+        </div>
+      </div>
+      <div className="card" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">Current location</h5>
+          <p className="card-text">{location.pathname}</p>
+        </div>
       </div>
     </div>
-    <div className="card" style={{ width: "18rem" }}>
-      <div className="card-body">
-        <h5 className="card-title">Current location</h5>
-        <p className="card-text">{location.pathname}</p>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
-const mapStateToProps = state => ({ formData: state });
-
-export default connect(mapStateToProps)(withRouter(ApplicationStats));
+export default ApplicationStats;
--- src/FormDisplay.js
+++ src/FormDisplay.js
@@ -1,89 +1,80 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import Field from "./Field";
 
-class FormDisplay extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      hobby: undefined,
-      idFlag: false,
-      id: 0
-    };
-    this.submitForm = this.submitForm.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  submitForm(evt) {
-    evt.preventDefault();
-    this.props.sendForm(this.state);
-  }
-  handleChange(evt) {
-    this.setState({
-      [evt.target.name]:
-        evt.target.type === "checkbox" ? evt.target.checked : evt.target.value
-    });
-  }
-  render() {
-    const { name, hobby, idFlag, id } = this.state;
-    return (
-      <div>
-        <form onSubmit={this.submitForm}>
-          <Field
-            type="text"
-            label="Full name"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-          />
-          <Field label="Hobbies" name="hobby">
-            <select
-              name="hobby"
-              id="hobby"
-              value={hobby}
-              onChange={this.handleChange}
-              class="form-control"
-            >
-              <option value="cooking">cooking</option>
-              <option value="running">running</option>
-              <option value="coding">coding</option>
-              <option value="driving">driving</option>
-            </select>
-          </Field>
-          <Field
-            type="checkbox"
-            label="Custom ID"
-            name="idFlag"
-            containerClass="form-check"
-            labelClass="form-check-label"
-            checked={idFlag}
-            onChange={this.handleChange}
-          />
-          {idFlag && (
-            <Field
-              label="ID"
-              type="number"
-              name="id"
-              value={id}
-              onChange={this.handleChange}
-            />
-          )}
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
-
-const sendFormAction = form => ({
+const sendForm = form => ({
   type: "SUBMIT_FORM",
   payload: form
 });
 
-const mapDispatchToProps = dispatch => ({
-  sendForm: form => dispatch(sendFormAction(form))
-});
-export default connect(null, mapDispatchToProps)(FormDisplay);
+const FormDisplay = ({ number }) => {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [hobby, setHobby] = useState();
+  const [idFlag, setIdFlag] = useState(false);
+  const [id, setId] = useState(0);
+
+  const submitForm = evt => {
+    evt.preventDefault();
+    dispatch(
+      sendForm({
+        name,
+        hobby,
+        idFlag,
+        ...(idFlag ? { id } : {})
+      })
+    );
+  };
+
+  return (
+    <div>
+      <h2>Form Example {number}</h2>
+      <form onSubmit={submitForm}>
+        <Field
+          type="text"
+          label="Full name"
+          name="name"
+          value={name}
+          onChange={evt => setName(evt.target.value)}
+        />
+        <Field label="Hobbies" id="hobby">
+          <select
+            name="hobby"
+            className="form-control"
+            value={hobby}
+            onChange={evt => setHobby(evt.target.value)}
+          >
+            <option value="cooking">cooking</option>
+            <option value="cooking">running</option>
+            <option value="cooking">coding</option>
+            <option value="cooking">driving</option>
+          </select>
+        </Field>
+        <Field
+          type="checkbox"
+          label="Custom ID"
+          name="idFlag"
+          containerClass="form-check custom-checkbox"
+          inputClass="form-check-input"
+          labelClass="form-check-label"
+          checked={idFlag}
+          onChange={evt => setIdFlag(evt.target.checked)}
+        />
+        {idFlag && (
+          <Field
+            label="ID"
+            type="number"
+            name="id"
+            value={id}
+            onChange={evt => setId(evt.target.value)}
+          />
+        )}
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FormDisplay;
